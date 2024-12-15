6ddcda720f327ee2248f97e4abf93a4bd0e4124f
shaikh-mohd-fahad <fahadazmilko@gmail.com>
2024-12-15 13:05:07 +0530

    image

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Image from 'next/image'
+import imgpath from "../../../public/trainer2.jpg"
 const page = () => {
   return (
     <div>
       this  is user dashboard
+      <Image src={imgpath} alt="temp photo" height={200} width={200}/>
     </div>
   )
 }
